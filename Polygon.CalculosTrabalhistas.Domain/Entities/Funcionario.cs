0cb5e0276a2f8c1694bbf20b11d38609f185225b
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-09-15 17:13:14 -0300

    Criação da entidade funcionario

---
+++ Polygon.CalculosTrabalhistas.Domain/Entities/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon.CalculosTrabalhistas.Domain.Entities
+{
+    public class Funcionario
+    {
+        protected Funcionario()
+        {
+
+        }
+
+        public Funcionario(int matricula, double valorHora)
+        {
+            Matricula = matricula;
+            ValorHora = valorHora;
+        }
+
+        public int Matricula { get; private set; }
+        public double ValorHora { get; private set; }
+    }
+}
