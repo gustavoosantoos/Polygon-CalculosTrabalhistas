e49ee13dd3333b62d5a87c6a9a2b44dbf6a31b6b
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-09-15 18:00:18 -0300

    Gitignore atualizado

---
+++ Polygon.CalculosTrabalhistas.Application/Interface/ICalculoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon.CalculosTrabalhistas.Application.Interface
+{
+    public interface ICalculoService
+    {
+        void RealizarCalculo();
+    }
+}
---
+++ Polygon.CalculosTrabalhistas.Application/Services/CalculoService.cs
@@ -0,0 +1,15 @@
+﻿using Polygon.CalculosTrabalhistas.Application.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon.CalculosTrabalhistas.Application.Services
+{
+    public class CalculoService : ICalculoService
+    {
+        public void RealizarCalculo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
