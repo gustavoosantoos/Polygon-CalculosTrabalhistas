79d60f28889ec2d7bbec9d68a0d3b3e49c359a34
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-09-15 17:48:06 -0300

    Criação do contrato de repositório e projeto de comunicações

---
+++ Polygon.CalculosTrabalhistas.Domain/Repositories/ICalculoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Polygon.CalculosTrabalhistas.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon.CalculosTrabalhistas.Domain.Repositories
+{
+    public interface ICalculoRepository
+    {
+        event EventHandler OnSalvarCalculo;
+        void Salvar(Calculo calculo);
+    }
+}
---
+++ Polygon.CalculosTrabalhistas.Repositories.Mongo/CalculoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Polygon.CalculosTrabalhistas.Domain.Entities;
+using Polygon.CalculosTrabalhistas.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon.CalculosTrabalhistas.Repositories.Mongo
+{
+    public class CalculoRepository : ICalculoRepository
+    {
+        public event EventHandler OnSalvarCalculo;
+
+        public void Salvar(Calculo calculo)
+        {
+            OnSalvarCalculo?.Invoke(this, null);
+            throw new NotImplementedException();
+        }
+    }
+}
