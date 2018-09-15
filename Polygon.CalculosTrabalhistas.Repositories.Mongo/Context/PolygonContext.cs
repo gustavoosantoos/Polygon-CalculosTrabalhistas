d1c6f2d91799d9acc274f58685efed5cc2ebb82d
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-09-15 18:32:50 -0300

    Criação da conexão com RavenDb

---
+++ Polygon.CalculosTrabalhistas.Repositories.Mongo/Context/PolygonContext.cs
@@ -0,0 +1,28 @@
+﻿using Raven.Client.Documents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon.CalculosTrabalhistas.Repositories.Raven.Context
+{
+    public class PolygonContext
+    {
+        private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
+
+        public static IDocumentStore Store
+        {
+            get { return store.Value; }
+        }
+
+        private static IDocumentStore CreateStore()
+        {
+            IDocumentStore store = new DocumentStore()
+            {
+                Urls = new string [] { "http://localhost:9001" },
+                Database = "Polygon"
+            }.Initialize();
+
+            return store;
+        }
+    }
+}
