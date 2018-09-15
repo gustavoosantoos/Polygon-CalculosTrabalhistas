455c48402a7a523013f010aa43b39abd8753e37b
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-09-15 17:17:04 -0300

    Criação da entidade cálculo

---
+++ Polygon.CalculosTrabalhistas.Domain/Entities/Calculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon.CalculosTrabalhistas.Domain.Entities
+{
+    public class Calculo
+    {
+        public Calculo(Funcionario funcionario, double horasTrabalhadas)
+        {
+            Funcionario = funcionario;
+            HorasTrabalhadas = horasTrabalhadas;
+            RealizaCalculos();
+        }
+
+        public Funcionario Funcionario { get; private set; }
+        public double HorasTrabalhadas { get; private set; }
+        public double SalarioBruto { get; private set; }
+        public double SalarioLiquido { get; private set; }
+        public double Inss { get; private set; }
+        public double ImpostoRenda { get; private set; }
+        
+        private void RealizaCalculos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
