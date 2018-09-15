90ec686a32b944f492dd3b3aebf918072dc0025e
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-09-15 17:26:04 -0300

    Método para cálculo de INSS simplificado

--- Polygon.CalculosTrabalhistas.Domain/Entities/Calculo.cs
+++ Polygon.CalculosTrabalhistas.Domain/Entities/Calculo.cs
@@ -10,19 +10,37 @@ namespace Polygon.CalculosTrabalhistas.Domain.Entities
         {
             Funcionario = funcionario;
             HorasTrabalhadas = horasTrabalhadas;
-            RealizaCalculos();
         }
 
         public Funcionario Funcionario { get; private set; }
         public double HorasTrabalhadas { get; private set; }
-        public double SalarioBruto { get; private set; }
-        public double SalarioLiquido { get; private set; }
+
+        public double SalarioBruto => Funcionario.ValorHora * HorasTrabalhadas;
         public double Inss { get; private set; }
         public double ImpostoRenda { get; private set; }
-        
-        private void RealizaCalculos()
+        public double SalarioLiquido => SalarioBruto - Inss - ImpostoRenda;
+
+
+        private double CalcularInss()
         {
-            throw new NotImplementedException();
+            const double DescontoMaximoInss = 621.04;
+
+            double CalculaDesconto(int porcentagem)
+            {
+                return (SalarioBruto / 100.0) * porcentagem;
+            }
+
+            switch (SalarioBruto)
+            {
+                case double salario when salario < 1693.72:
+                    return CalculaDesconto(8);
+                case double salario when salario < 2822.90:
+                    return CalculaDesconto(9);
+                case double salario when salario < 5645.80:
+                    return CalculaDesconto(11);
+                default:
+                    return DescontoMaximoInss;
+            }
         }
     }
 }
